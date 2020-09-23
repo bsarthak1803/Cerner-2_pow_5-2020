604891a778d6319c0d9e32d0611c54aa4bff1b3e
sarthak bhardwaj <sarthak.bhardwaj@cerner.com>
2020-09-23 14:11:11 +0530

    lines reduced to 32

--- nine.hs
+++ nine.hs
@@ -2,7 +2,6 @@
 --https://tio.run/#haskell to run the program (provide required inputs in the input section)
 putTodo :: (Int, String) -> IO ()
 putTodo (n, todo) = putStrLn (show n ++ ": " ++ todo)
-
 prompt :: [String] -> IO ()
 prompt todos = do
     putStrLn ""
@@ -10,7 +9,6 @@ prompt todos = do
     mapM_ putTodo (zip [0..] todos)
     command <- getLine
     interpret command todos
-
 interpret :: String -> [String] -> IO ()
 interpret ('+':' ':todo) todos = prompt (todo:todos)
 interpret ('-':' ':num ) todos =
@@ -22,14 +20,12 @@ interpret  "q"           todos = return ()
 interpret  command       todos = do
     putStrLn ("Invalid command: `" ++ command ++ "`")
     prompt todos
-
 delete :: Int -> [a] -> Maybe [a]
 delete 0 (_:as) = Just as
 delete n (a:as) = do
     as' <- delete (n - 1) as
     return (a:as')
 delete _  []    = Nothing
-
 main = do
     putStrLn "Commands:"
     putStrLn "+ <String> - Add a TODO entry"
